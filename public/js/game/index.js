555ca5af7a6f0adfd2a70b8412d04976242ea8da
Miqah <ghina@MacBook-Air-ghina.local>
2023-09-17 21:49:23 +0700

    save perubahan

--- public/js/game/index.js
+++ public/js/game/index.js
@@ -1,28 +1,28 @@
-function Index(){
-	var socket = false;
-	
-	this.start = function(){
-		socket = io();
-		this.bindSocketEvents();
-		socket.emit('index_init');
-	}
-	
-	this.bindSocketEvents = function(){
-		socket.on('index_init_ok',function(data){
-			var html = "";
-			
-			for(var i in data){
-				var q = data[i];
-				
-				html += "<div><a href='"+q.link+"'>* "+q.quiz+"</a></div>";
-			}
-			
-			$('#quiz_list_area').html(html);
-		});
-	}
+function Index() {
+  var socket = false;
+
+  this.start = function () {
+    socket = io();
+    this.bindSocketEvents();
+    socket.emit("index_init");
+  };
+
+  this.bindSocketEvents = function () {
+    socket.on("index_init_ok", function (data) {
+      var html = "";
+
+      for (var i in data) {
+        var q = data[i];
+
+        html += `<div class="list__soal_card"><a href='${q.link}' class='nama-kelas-css'>${q.quiz}</a></div>`;
+      }
+
+      $("#quiz_list_area").html(html);
+    });
+  };
 }
 
-$(document).ready(function(){
-	var index = new Index();
-	index.start();
+$(document).ready(function () {
+  var index = new Index();
+  index.start();
 });
